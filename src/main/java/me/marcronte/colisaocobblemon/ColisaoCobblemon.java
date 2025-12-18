e91bbe8060f66fcf5ac3af9a1d667304a3b44145
João Marcus Braga dos Santos <79426328+joaombsantos@users.noreply.github.com>
2025-12-18 00:42:15 -0300

    initial commit

---
+++ src/main/java/me/marcronte/colisaocobblemon/ColisaoCobblemon.java
@@ -0,0 +1,16 @@
+package me.marcronte.colisaocobblemon;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ColisaoCobblemon implements ModInitializer {
+	public static final String MOD_ID = "colisao-cobblemon";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/me/marcronte/colisaocobblemon/ColisaoCobblemonDataGenerator.java
@@ -0,0 +1,11 @@
+package me.marcronte.colisaocobblemon;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ColisaoCobblemonDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
